5a7efbdbaed3d51237d9ddb9a0bc1d02059f8325
stephenkeep <stephen@keep-creative.com>
2016-10-30 11:00:51 +0000

    add brightness controls to white bulb

--- MiLight.js
+++ MiLight.js
@@ -1,51 +1,64 @@
-var Milight = require('node-milight-promise').MilightController;
-var commands = require('node-milight-promise').commands;
+const Milight = require('node-milight-promise').MilightController
+const commands = require('node-milight-promise').commands
 
-module.exports = function(RED) {
-    function node (config) {
-        
-        RED.nodes.createNode(this, config);
-        var node = this;
-                        
-        this.on('input', function(msg) {
-            
-            var light = new Milight({
-                    ip: config.ip,
-                    delayBetweenCommands: 70,
-                    commandRepeat: config.bulbtype === 'rgbw' ? 2 : 1
-                }),
-                zone = config.zone,
-                bulb = config.bulbtype;
+module.exports = function (RED) {
+  function node (config) {
+    RED.nodes.createNode(this, config)
+    this.on('input', function (msg) {
+      const light = new Milight({
+        ip: config.ip,
+        delayBetweenCommands: 70,
+        commandRepeat: config.bulbtype === 'rgbw' ? 2 : 1
+      })
 
-            //i know this is private but its the only way i could get it to work
-            light._broadcastMode = config.broadcast;
-            
-            if (msg.payload === 'off') light.sendCommands(commands[bulb].off(zone));
-            if (msg.payload === 'on') light.sendCommands(commands[bulb].on(zone));
-            
-            if (bulb === 'rgbw') {
-                
-                if (msg.payload === 'disco') {
-                    
-                    light.sendCommands(commands.rgbw.on(zone));
-                    for (var x = 0; x < 256; x += 5) {
-                        light.sendCommands(commands.rgbw.hue(x));
-                        light.pause(100);
-                    }
-                }
-                
-                if (msg.payload === 'white') light.sendCommands(commands.rgbw.on(zone), commands.rgbw.brightness(100), commands.rgbw.whiteMode(zone));
-                
-                if (!isNaN(msg.payload)) {
-                    light.sendCommands(commands.rgbw.hue(msg.payload));
-                }
-            }
-            
-            light.close().then(function () {
-                
-            });
-        });
-    };
- 
-    RED.nodes.registerType("MiLight",node);
+      const zone = config.zone
+      const bulb = config.bulbtype
+      const brightness = msg.brightness || 100
+
+      // i know this is private but its the only way i could get it to work
+      light._broadcastMode = config.broadcast
+
+      if (msg.payload === 'off') {
+        light.sendCommands(commands[bulb].off(zone))
+      }
+
+      if (msg.payload === 'on') {
+        light.sendCommands(commands[bulb].on(zone))
+      }
+
+      if (msg.payload === 'brightUp') {
+        light.sendCommands(commands[bulb].brightUp())
+      }
+
+      if (msg.payload === 'brightDown') {
+        light.sendCommands(commands[bulb].brightDown())
+      }
+
+      if (msg.payload === 'maxBright') {
+        light.sendCommands(commands[bulb].maxBright())
+      }
+
+      if (bulb === 'rgbw') {
+        if (msg.payload === 'disco') {
+          light.sendCommands(commands.rgbw.on(zone))
+          for (var x = 0; x < 256; x += 5) {
+            light.sendCommands(commands.rgbw.hue(x))
+            light.pause(100)
+          }
+        }
+
+        if (msg.payload === 'white') {
+          light.sendCommands(commands.rgbw.on(zone), commands.rgbw.brightness(brightness), commands.rgbw.whiteMode(zone))
+        }
+
+        if (!isNaN(msg.payload)) {
+          light.sendCommands(commands.rgbw.hue(msg.payload))
+        }
+      }
+
+      light.close().then(function () {})
+    })
+  }
+
+  RED.nodes.registerType('MiLight', node)
 }
